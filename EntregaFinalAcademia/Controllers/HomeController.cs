db3f9e7eb2958d8e8da95eaeddccecde3febc838
PabloM <pablomussanti@hotmail.com>
2023-08-28 09:41:00 -0300

    Agregar archivos de proyecto.

---
+++ EntregaFinalAcademia/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntregaFinalAcademia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
